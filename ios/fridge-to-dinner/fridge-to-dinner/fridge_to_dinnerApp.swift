43dc0eba241cf9b21d61571fa54d3fd2a286de79
Darius Sibarium <dsibarium@gmail.com>
2026-06-27 17:23:28 +0200

    Add SwiftUI app scaffold

---
+++ ios/fridge-to-dinner/fridge-to-dinner/fridge_to_dinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fridge_to_dinnerApp.swift
+//  fridge-to-dinner
+//
+//  Created by Darius Sibarium on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct fridge_to_dinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
